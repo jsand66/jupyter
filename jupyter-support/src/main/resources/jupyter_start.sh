5f452fe95941ab0e304e9de94e2494c30e496dec
Gangadhar Ambati <gambati@cpex.com>
2019-10-23 18:37:12 +0530

    updated shell scripts

--- jupyter-support/src/main/resources/jupyter_start.sh
+++ jupyter-support/src/main/resources/jupyter_start.sh
@@ -4,10 +4,10 @@ if [ -z "$check_image" ];then
         cd /home/entptools/jupyter-zone/
         build=`sudo docker build -t jupyter-datascience:base .`
         cont=`sudo docker run -d -p 8000:8888 jupyter-datascience:base`
-		copy_workspace=`docker cp -r /var/opt/entptools/pcapquery/jupyter-workspace/* $cont:/home/entptools/`
+		copy_workspace=`sudo docker cp /var/opt/entptools/pcapquery/jupyter-workspace/. $cont:/home/entptools/`
         echo "$cont"
 else
         cont=`sudo docker run -d -p 8000:8888 jupyter-datascience:base`
-		copy_workspace=`docker cp -r /var/opt/entptools/pcapquery/jupyter-workspace/* $cont:/home/entptools/`
+		copy_workspace=`sudo docker cp /var/opt/entptools/pcapquery/jupyter-workspace/. $cont:/home/entptools/`
         echo "$cont"
 fi
