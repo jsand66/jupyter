c0af0d4f273b6b7a974e623f3a2cc508b8b0090f
Gangadhar Ambati <gambati@cpex.com>
2019-10-23 16:23:42 +0530

    updated files

--- jupyter-support/src/main/resources/jupyter_start.sh
+++ jupyter-support/src/main/resources/jupyter_start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-check_image=`docker images|grep -w "jupyter/datascience-notebook"`
-if [ -z check_image ];then
-	cd /home/entptools/jupyter_zone/
-	build=`docker build -t jupyter-datascience:base .`
-	cont=`sudo docker run -d -p 8000:8888 jupyter-datascience:base`
-	echo "$cont"
+check_image=`sudo docker images|grep -w "jupyter-datascience"`
+if [ -z "$check_image" ];then
+        cd /home/entptools/jupyter-zone/
+        build=`sudo docker build -t jupyter-datascience:base .`
+        cont=`sudo docker run -d -p 8000:8888 jupyter-datascience:base`
+        echo "$cont"
 else
-	cont=`sudo docker run -d -p 8000:8888 jupyter-datascience:base`
-	echo "$cont"
+        cont=`sudo docker run -d -p 8000:8888 jupyter-datascience:base`
+        echo "$cont"
 fi
