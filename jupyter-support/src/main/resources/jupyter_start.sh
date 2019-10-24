2a7e15cc6d8f9e69a2f3aa86c1b621b103a04566
Gangadhar Ambati <gambati@cpex.com>
2019-10-24 15:39:36 +0530

    updated jupyter_start.sh

--- jupyter-support/src/main/resources/jupyter_start.sh
+++ jupyter-support/src/main/resources/jupyter_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 check_image=`sudo docker images|grep -w "jupyter-datascience"`
 if [ -z "$check_image" ];then
-        cd /home/entptools/jupyter-zone/
+        cd /var/opt/tomcat-7.0.54/theice.com/entpmonitor/webapps/jupyter-support/WEB-INF/classes/
         build=`sudo docker build -t jupyter-datascience:base .`
         cont=`sudo docker run -d -p 8000:8888 jupyter-datascience:base`
 		copy_workspace=`sudo docker cp /var/opt/entptools/pcapquery/jupyter-workspace/. $cont:/home/entptools/`
