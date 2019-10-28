2ad1e0ea18a9aefe6b211862a8c5b5340feacb5c
Gangadhar Ambati <gambati@cpex.com>
2019-10-28 10:26:41 +0530

    updated scripts

--- jupyter-support/src/main/java/com/jupyter/RemoteScript.java
+++ jupyter-support/src/main/java/com/jupyter/RemoteScript.java
@@ -47,7 +47,7 @@ public class RemoteScript {
 	public String stopJupyter(String container_id) {
 		try {
 			System.out.println("contid:" + container_id);
-			String out=saveWorkspace(container_id);
+			// String out=saveWorkspace(container_id);
 			File file = new File(getClass().getClassLoader().getResource("jupyter_stop.sh").getFile());
 			String cmd = "sh " + file + " " + container_id;
 			System.out.println("cmd:" + cmd);
@@ -134,7 +134,7 @@ public class RemoteScript {
 		}
 		return output;
 	}
-	
+
 	public String saveWorkspace(String container_id) {
 		String output = null;
 
@@ -158,13 +158,14 @@ public class RemoteScript {
 		}
 		return output;
 	}
-	
-	public String checkContainer(String container_id) {
+
+	public JSONObject checkContainer() {
 		String output = null;
+		JSONObject object = new JSONObject();
 
 		try {
 			File file = new File(getClass().getClassLoader().getResource("check_container.sh").getFile());
-			String cmd = "sh " + file + " " + container_id;
+			String cmd = "sh " + file;
 			Runtime run = Runtime.getRuntime();
 			Process pr = run.exec(cmd);
 			pr.waitFor();
@@ -174,13 +175,27 @@ public class RemoteScript {
 				output = line;
 				System.out.println("out " + output);
 			}
+			if (output.contains("no")) {
+				object.put("status","no");
+			}
+			else {
+				String[] arr = output.split(",");
+				System.out.println("port:" + arr[2]);
+				System.out.println("id:" + arr[0]);
+				System.out.println("name:" + arr[1]);
+				object.put("status", "yes");
+				object.put("container_id", arr[0]);
+				object.put("container_name", arr[1]);
+				object.put("port", arr[2]);
+				
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		return output;
+		return object;
 	}
 
 }
