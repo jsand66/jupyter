d5caff6076ab37c7a2b8e9700e222d624558a09b
Gangadhar Ambati <gambati@cpex.com>
2019-11-13 16:46:49 +0530

    updated scripts to docker compose

--- jupyter-support/src/main/resources/jupyter_start.sh
+++ jupyter-support/src/main/resources/jupyter_start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-check_image=`sudo docker images|grep -w "jupyter-datascience"`
-if [ -z "$check_image" ];then
-        cd /var/opt/tomcat-7.0.54/theice.com/entpmonitor/webapps/jupyter-support/WEB-INF/classes/
-        build=`sudo docker build -t jupyter-datascience:base .`
-        cont=`sudo docker run -d -p 8000:8888 -v /var/opt/entptools/pcapquery/jupyter-workspace:/home/entptools jupyter-datascience:base`
-        echo "$cont"
-else
-        cont=`sudo docker run -d -p 8000:8888 -v /var/opt/entptools/pcapquery/jupyter-workspace:/home/entptools jupyter-datascience:base`
-        echo "$cont"
-fi
+cd /var/opt/tomcat-7.0.54/theice.com/entpmonitor/webapps/jupyter-support/WEB-INF/classes/
+check_image=`sudo docker-compose up --build -d`
+cont=`sudo docker ps --no-trunc|grep jupyter-container|awk '{print $1}'`
+echo "$cont"
